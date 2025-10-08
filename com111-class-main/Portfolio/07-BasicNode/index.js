c2141a03f1cf4eec76cb58a7b2bfe5164b80c2f1
Rohyoh <mrcampoy.m@gmail.com>
2025-10-08 12:28:42 -0600

    Progress on ACT7

---
+++ com111-class-main/Portfolio/07-BasicNode/index.js
@@ -0,0 +1,11 @@
+//Hola mundo
+console.log("Hola mundo")
+//Quote
+const sw = require('star-wars-quotes')
+console.log(sw())
+//Superhero & Supervillain
+
+//Secret message
+/*const fs = require('fs');
+let secretMessage = fs.readFileSync('./data/input.txt', 'utf8');
+console.log(secretMessage);*/
